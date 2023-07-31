30bd45250062c2ec296b86016e280abbd25bee42
Rodrigo_Elguera <111022228+RodrigoEdit@users.noreply.github.com>
2023-07-31 17:15:26 +0000

    commit

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -7,38 +7,38 @@ class App extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {employees: []};
+		this.state = {instrumentos: []};
 	}
 
 	componentDidMount() { 
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
+		client({method: 'GET', path: '/api/instrumentos'}).done(response => {
+			this.setState({employees: response.entity._embedded.instrumentos});
 		});
 	}
 
 	render() { 
 		return (
-			<EmployeeList employees={this.state.employees}/>
+			<instrumentosList instrumentos={this.state.instrumentos}/>
 		)
 	}
 }
 
 
 
-class EmployeeList extends React.Component{
+class InstrumentosList extends React.Component{
 	render() {
-		const employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
+		const instrumentos = this.props.instrumentos.map(instrumento =>
+			<Instrumentos key={instrumento._links.self.href} instrumento={instrumento}/>
 		);
 		return (
 			<table>
 				<tbody>
 					<tr>
-						<th>First Name</th>
-						<th>Last Name</th>
-						<th>Description</th>
+						<th>Nombre</th>
+						<th>Categoria</th>
+						<th>Descripcion</th>
 					</tr>
-					{employees}
+					{instrumentos}
 				</tbody>
 			</table>
 		)
@@ -49,9 +49,9 @@ class Employee extends React.Component{
 	render() {
 		return (
 			<tr>
-				<td>{this.props.employee.firstName}</td>
-				<td>{this.props.employee.lastName}</td>
-				<td>{this.props.employee.description}</td>
+				<td>{this.props.instrumento.Nombre}</td>
+				<td>{this.props.instrumento.Categoria}</td>
+				<td>{this.props.instrumento.Descripcion}</td>
 			</tr>
 		)
 	}
